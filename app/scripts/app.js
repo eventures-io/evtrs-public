47138ffc672c4d596c466c8217051ea05241d76b
Peter Symoens <peter.symoens@eventures.io>
2014-09-21 21:44:25 +0200

    switched to ui-router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,30 +1,79 @@
-var animateApp = angular.module('animateApp', ['ngRoute', 'ngAnimate']);
+'use strict'
+var eventApp = angular.module('eventApp', ['ui.router','ngAnimate']);
 
-animateApp.config(function($routeProvider) {
-    $routeProvider
-    	.when('/', {
-    		templateUrl: 'views/page-home.html',
-            controller: 'mainController'
-    	})
-    	.when('/about', {
-    		templateUrl: 'views/page-about.html',
-            controller: 'aboutController'
-    	})
-    	.when('/contact', {
-    		templateUrl: 'views/page-contact.html',
-            controller: 'contactController'
-    	});
+// set the configuration
+eventApp.run(['$rootScope', '$state', '$stateParams', '$window', function ($rootScope, $state, $stateParams, $window) {
 
-});
+    $rootScope.$on('$stateChangeError',
+        function(event, toState, toParams, fromState, fromParams, error){
+            console.log('tostate error: ' + toState.url);
+        });
 
-animateApp.controller('mainController', function($scope) {
-    $scope.pageClass = 'page-home';
-});
+    $rootScope.$on('$stateChangeStart',
+        function (event, toState, toParams, fromState, fromParams) {
+//            event.preventDefault();
+            console.log('changing from ' + fromState.url + ' to ' +  toState.url  );
+        });
 
-animateApp.controller('aboutController', function($scope) {
-    $scope.pageClass = 'page-about';
-});
+//    $rootScope.$on('$stateChangeSuccess',
+//        function (event, toState, toParams, fromState, fromParams) {
+//            console.log('changed from ' + fromState.url + ' to ' +  toState.url  );
+//
+//        });
+//
+//    $rootScope.$on('$stateNotFound',
+//        function(event, unfoundState, fromState, fromParams){
+//            console.log('state not found');
+//            console.log(unfoundState.to); // "lazy.state"
+//            console.log(unfoundState.toParams); // {a:1, b:2}
+//            console.log(unfoundState.options); // {inherit:false} + default options
+//        })
+}]);
 
-animateApp.controller('contactController', function($scope) {
-    $scope.pageClass = 'page-contact';
-});
+
+eventApp.config(function ($locationProvider, $stateProvider, $urlRouterProvider) {
+
+    $locationProvider.html5Mode(true).hashPrefix('!');
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+        .state('intro', {
+            url: '/',
+            templateUrl: 'views/partials/intro.part.html'
+        })
+        .state('base', {
+            abstract: true,
+            url: "/",
+            templateUrl: 'views/partials/base.part.html'
+        })
+        .state('base.services', {
+            url: "services",
+            views: {
+                'content-view@base': { templateUrl: 'views/partials/services.part.html',
+                    controller: 'ServicesController'
+                }
+            }
+        })
+        .state('base.about', {
+            url: "about",
+            views: {
+                'content-view@base': { templateUrl: 'views/partials/about.part.html'
+                }
+            }
+        })
+
+})
+;
+
+
+eventApp.controller ("ServicesController", function ($scope) {
+    $scope.$on('$viewContentLoaded',
+        function (event) {
+            //alert (" in controller ");
+
+        });
+})
+
+
+
